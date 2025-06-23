9aeaf7995787ab77b3f0039158b7c2a9a20a50f0
ChammakhiSara <sarachammakhi77@gmail.com>
2025-06-23 12:00:15 +0100

    project fixed + methods implementations + unit test fixed

--- src/main/java/org/machinestalk/api/impl/UserApiImpl.java
+++ src/main/java/org/machinestalk/api/impl/UserApiImpl.java
@@ -3,28 +3,52 @@ package org.machinestalk.api.impl;
 import org.machinestalk.api.UserApi;
 import org.machinestalk.api.dto.UserDto;
 import org.machinestalk.api.dto.UserRegistrationDto;
+import org.machinestalk.domain.User;
 import org.machinestalk.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 @RestController
 public class UserApiImpl implements UserApi {
 
     private final UserService userService;
+    private final ModelMapper mapper;
 
-    public UserApiImpl(final UserService userService) {
+    public UserApiImpl(final UserService userService, final ModelMapper mapper) {
         this.userService = userService;
+        this.mapper = mapper;
     }
 
     @Override
     public UserDto register(final UserRegistrationDto userRegistrationDto) {
         // implement me !!
-        return null;
+        User user = userService.registerUser(userRegistrationDto);
+        return mapper.map(user, UserDto.class);
     }
 
     @Override
     public Mono<UserDto> findUserById(long id) {
         // implement me !!
-        return null;
+        return userService.getById(id)
+                .map(user -> {
+                    UserDto.UserInfos infos = new UserDto.UserInfos(
+                            user.getFirstName(),
+                            user.getLastName(),
+                            user.getDepartment() != null ? user.getDepartment().getName() : null,
+                            user.getAddresses() != null
+                                    ? user.getAddresses().stream()
+                                    .map(a -> String.format("%s %s, %s %s, %s",
+                                            a.getStreetNumber(), a.getStreetName(),
+                                            a.getPostalCode(), a.getCity(),
+                                            a.getCountry()))
+                                    .collect(Collectors.toList())
+                                    : Collections.emptyList()
+                    );
+                    return new UserDto(String.valueOf(user.getId()), infos);
+                });
     }
 }
--- src/main/java/org/machinestalk/service/impl/UserServiceImpl.java
+++ src/main/java/org/machinestalk/service/impl/UserServiceImpl.java
@@ -3,21 +3,31 @@ import org.machinestalk.api.dto.UserRegistrationDto;
 import org.machinestalk.domain.User;
 import org.machinestalk.repository.UserRepository;
 import org.machinestalk.service.UserService;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+@Service
 public class UserServiceImpl implements UserService {
 
   private UserRepository userRepository;
+  private final ModelMapper mapper;
+
+  public UserServiceImpl(UserRepository userRepository, ModelMapper mapper) {
+    this.userRepository = userRepository;
+    this.mapper = mapper;
+  }
 
   @Override
   public User registerUser(final UserRegistrationDto userRegistrationDto) {
     // implement me !!
-    return null;
+    User user = this.mapper.map(userRegistrationDto, User.class);
+    return userRepository.save(user);
   }
 
   @Override
   public Mono<User> getById(final long id) {
     // implement me !!
-    return null;
+    return Mono.justOrEmpty(userRepository.findById(id));
   }
 }
